f039b7200e0c7da78c102ebdb4fd8289207e499c
Achim Friedland <github@ahzf.de>
2011-08-28 14:55:28 +0200

    HTML5 lib created.

---
+++ HTML5Demo/HTML5Demo.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright (c) 2011 Achim 'ahzf' Friedland <achim@ahzf.de>
+ * This file is part of Loki <http://www.github.com/ahzf/Loki>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * You may obtain a copy of the License at
+ *     http://www.gnu.org/licenses/gpl.html
+ *     
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.ComponentModel;
+using de.ahzf.Blueprints;
+using de.ahzf.Blueprints.PropertyGraph;
+
+#endregion
+
+namespace de.ahzf.Loki
+{
+
+    public class HTML5Demo
+    {
+        
+        public static void Main(string[] args)
+        {
+        }
+
+    }
+
+}
