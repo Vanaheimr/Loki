f546138e7ba8893ff32b0d3afb7e7110f084984c
Achim Friedland <github@ahzf.de>
2013-08-27 23:35:20 +0200

    Some minor updates

--- GraphCanvas.WPF/Properties/Resources.Designer.cs
+++ GraphCanvas.WPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GraphCanvas.Properties {
+namespace eu.Vanaheimr.GraphCanvas.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GraphCanvas.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GraphCanvas.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("eu.Vanaheimr.GraphCanvas.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- GraphCanvas.WPF/Properties/Settings.Designer.cs
+++ GraphCanvas.WPF/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GraphCanvas.Properties {
+namespace eu.Vanaheimr.GraphCanvas.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
